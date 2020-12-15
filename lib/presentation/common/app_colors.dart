b463f4464309ca4a1191fcc5b44dc8933e860680
Brayan Cantos <brayancantosm07ci@gmail.com>
2020-12-14 23:54:08 -0500

    Add Packages - Add Cap Presentation

---
+++ lib/presentation/common/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/painting.dart' show Color;
+
+class AppColors {
+  static final blue = Color(0xFF3F90F6);
+  static final orange = Color(0xFFED8306);
+  static final grey = Color(0xFF7A7A7A);
+  static final greyDark = Color(0xFF404040);
+  static final greyLight = Color(0xFFC4C4C4);
+}
---
+++ lib/presentation/i_need/i_need_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class INeedPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Hola Mundo'),
+      ),
+    );
+  }
+}
